0c74e14a230c65bcb2c11c7387131331a6df607c
coderTomato <570547651@qq.com>
2020-02-15 18:12:53 +0800

    提交服务提供者工程

---
+++ msc-provider8081/src/main/java/com/jd/MscProvider8081Application.java
@@ -0,0 +1,13 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscProvider8081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscProvider8081Application.class, args);
+    }
+
+}
---
+++ msc-provider8081/src/main/java/com/jd/service/DepartServiceImpl.java
@@ -0,0 +1,53 @@
+package com.jd.service;
+
+import com.jd.bean.Depart;
+import com.jd.repository.DepartRepository;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartServiceImpl implements DepartService {
+
+    @Autowired
+    private DepartRepository repository;
+
+    @Override
+    public boolean saveDepart(Depart depart) {
+        Depart obj = repository.save(depart);
+        return obj != null? true : false;
+    }
+
+    @Override
+    public boolean removeDepartById(Integer id) {
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean modifyDepart(Depart depart) {
+        Depart obj = repository.save(depart);
+        return obj != null? true : false;
+    }
+
+    @Override
+    public Depart getDepartById(int id) {
+        if(repository.existsById(id)){
+            return repository.getOne(id);
+        }
+        Depart depart = new Depart();
+        depart.setName("no this depart");
+        return depart;
+    }
+
+    @Override
+    public List<Depart> listAllDeparts() {
+
+        return repository.findAll();
+    }
+}
