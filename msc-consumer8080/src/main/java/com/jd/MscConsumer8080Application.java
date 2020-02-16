8657a30ec6582a8f55aea66eb699b836b0e68791
coderTomato <570547651@qq.com>
2020-02-16 09:19:02 +0800

    提交消费者工程

---
+++ msc-consumer8080/src/main/java/com/jd/MscConsumer8080Application.java
@@ -0,0 +1,13 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscConsumer8080Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscConsumer8080Application.class, args);
+    }
+
+}
