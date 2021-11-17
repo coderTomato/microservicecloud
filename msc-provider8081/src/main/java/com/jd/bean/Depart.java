b38da52cc8852b72d7a8eda041f3dbaa7aeb9823
coderTomato <570547651@qq.com>
2021-11-17 12:33:03 +0800

    init

--- msc-provider8081/src/main/java/com/jd/bean/Depart.java
+++ msc-provider8081/src/main/java/com/jd/bean/Depart.java
@@ -7,10 +7,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
+
+@Data //lombok
 @Entity //使用自动建表
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler", "fieldHandler"}) //忽略hibernate延迟加载等特性
 public class Depart {
+
     @Id //表示当前属性为自动建的表的主键
     @GeneratedValue(strategy= GenerationType.IDENTITY)//主键自动递增
     private Integer id;
